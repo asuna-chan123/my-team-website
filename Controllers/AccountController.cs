dcea4b2f2140fcfb19fabe1b92005ee89f1412a5
avgfantasy-cyber <avgfantasy@gmail.com>
2025-10-14 03:58:35 +0700

    Initial commit - Add MVC project with Login, Cart, Checkout

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AdidasStoreMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: /Account/LoginSignup
+        public ActionResult LoginSignup()
+        {
+            return View();
+        }
+
+        // POST: /Account/LoginSignup
+        [HttpPost]
+        public ActionResult LoginSignup(string Email)
+        {
+            ViewBag.Message = "Cảm ơn bạn đã đăng ký: " + Email;
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AdidasStoreMVC.MvcApplication" Language="C#" %>
